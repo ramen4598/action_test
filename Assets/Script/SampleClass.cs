a7792c8fb2cea04b340fd6b54e0c19941d3731b0
tired_i <ramen4598@naver.com>
2023-06-25 01:09:21 +0900

    test code 추가

---
+++ Assets/Script/SampleClass.cs
@@ -0,0 +1,22 @@
+/// <summary>
+/// This is a Doxygen comment for the SampleClass.
+/// </summary>
+public class SampleClass
+{
+    /// <summary>
+    /// This is a Doxygen comment for the constructor of SampleClass.
+    /// </summary>
+    public SampleClass()
+    {
+    }
+
+    /// <summary>
+    /// This is a Doxygen comment for the DoSomething method.
+    /// </summary>
+    /// <param name="param1">This is a parameter.</param>
+    /// <returns>Returns an integer.</returns>
+    public int DoSomething(int param1)
+    {
+        return param1;
+    }
+}
