c2d133366a99ec3b5cba63cf6742303adf9d54b0
tired_i <ramen4598@naver.com>
2023-06-25 18:51:59 +0900

    test

--- Assets/Script/SampleClass.cs
+++ Assets/Script/SampleClass.cs
@@ -9,9 +9,9 @@ public class SampleClass
     }
 
     /**
-    *
-    * @param param1 This is a parameter.</param>
-    * @return int Returns an integer.</returns>
+    * @breif please do something!!
+    * @param param1 This is a parameter.
+    * @return int Returns an integer.
     */
     public int DoSomething(int param1)
     {
